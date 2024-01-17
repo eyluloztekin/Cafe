371d0ac1161867ae01023918c441ccaa005cc4c6
Mehmet Ali Tilgen <156668456+mehmetalitilgen@users.noreply.github.com>
2024-01-17 13:41:55 +0300

    Add files via upload

--- deneme.py
+++ deneme.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+from PIL import ImageTk, Image
 
 window = tk.Tk()
 window.config(background="azure4")
@@ -6,14 +7,53 @@ window.title("Kotta-Coffee")
 window.geometry("{}x{}+0+0".format(window.winfo_screenwidth(), window.winfo_screenheight()))
 
 
-def new_pencere(button_text):
-    # new_window = tk.Toplevel(window)
-    # new_window.geometry("600x825+{}+{}".format(window.winfo_screenwidth() - 700, window.winfo_screenheight() - 950))
-    # new_window.config(pady=50,background="aquamarine3")
-    # new_window.title(button_text)
+# Load the image
 
+def new_pencere(button_text):
     receipt_label = tk.Label(window, text=button_text, background="aquamarine3", width=70, height=50, anchor="n")
     receipt_label.place(x=1200, y=100)
+    urun_button = tk.Button(window, text="ürün ekle", background="aquamarine3", width=12, height=4)
+    urun_button.pack()
+    urun_button.place(x=window.winfo_screenwidth() - 2230, y=200)
+    urun_sil = tk.Button(window, text="ürün sil", background="aquamarine3", width=12, height=4)
+    urun_sil.pack()
+    urun_sil.place(x=window.winfo_screenwidth() - 2230, y=300)
+    hesap_al_button = tk.Button(window, text="Hesap Al", command=lambda: show_payment_options(button_text),
+                                background="aquamarine3", width=12, height=4)
+    hesap_al_button.pack()
+    hesap_al_button.place(x=window.winfo_screenwidth() - 2630, y=850)
+
+
+def show_payment_options(button_text):
+    global kredi_karti_button, nakit_button
+    nakit_image = Image.open("para.png")
+    resized_image1 = nakit_image.resize((120, 85))
+    image_nakit = ImageTk.PhotoImage(resized_image1)
+    nakit_button = tk.Button(window, image=image_nakit,
+
+                             command=lambda: hesap_ode(button_text, "Kredi Kartı"),
+                             background="aquamarine3", width=120, height=75)
+    nakit_button.image = image_nakit
+    nakit_button.pack()
+    nakit_button.place(x=window.winfo_screenwidth() - 2500, y=850)
+
+    # Load and resize the image
+    original_image = Image.open("kredi.png")
+    resized_image = original_image.resize((120, 50))
+    image_kredi = ImageTk.PhotoImage(resized_image)
+
+    kredi_karti_button = tk.Button(window, image=image_kredi,
+
+                                   command=lambda: hesap_ode(button_text, "Kredi Kartı"),
+                                   background="aquamarine3", width=120, height=75)
+    kredi_karti_button.image = image_kredi
+    kredi_karti_button.pack()
+    kredi_karti_button.place(x=window.winfo_screenwidth() - 2370, y=850)
+
+
+def hesap_ode(button_text, payment_method):
+    kredi_karti_button.destroy()
+    nakit_button.destroy()
 
 
 def button_click(button_text):
